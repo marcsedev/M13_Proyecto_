56d0843b3eb924fac808d5b171cc428612f0f445
MarcSogas <marc.sogas@gmail.com>
2022-05-30 18:17:47 +0200

    1a versión

---
+++ app/src/main/java/com/example/m13_proyecto/adapter/AlumnadoAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.m13_proyecto.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.m13_proyecto.Alumnado
+import com.example.m13_proyecto.R
+
+class AlumnadoAdapter ( private val alumnadoList:List<Alumnado>,) : RecyclerView.Adapter<AlumnadoViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumnadoViewHolder {
+        val layoutInflater=LayoutInflater.from(parent.context)
+        return AlumnadoViewHolder(layoutInflater.inflate(R.layout.item_alumnado, parent,false))
+    }
+
+    override fun onBindViewHolder(holder: AlumnadoViewHolder, position: Int) {
+        val item = alumnadoList[position]
+        holder.render(item)
+    }
+
+    override fun getItemCount(): Int =alumnadoList.size
+
+
+}
+
+
