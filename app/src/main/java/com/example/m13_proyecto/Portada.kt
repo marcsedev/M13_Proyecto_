83b90fdd7cc992ffe17caa60cba00457e291f8b6
MarcSogas <marc.sogas@gmail.com>
2022-05-30 12:40:46 +0200

    1a versión

---
+++ app/src/main/java/com/example/m13_proyecto/Portada.kt
@@ -0,0 +1,38 @@
+package com.example.m13_proyecto
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.m13_proyecto.databinding.ActivityPortadaBinding
+
+class Portada : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPortadaBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(1000)
+        super.onCreate(savedInstanceState)
+        binding= ActivityPortadaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //se inician las funciones
+        iniciarsesion()
+        registrarse()
+
+    }
+    fun iniciarsesion() {
+        //funcion para inciar sesion
+        binding.btIniciarSesion.setOnClickListener {
+        val lanzar = Intent(this, MainActivity::class.java)
+        startActivity((lanzar))
+
+        }
+    }
+
+    fun registrarse() {
+        binding.btRegistrarse.setOnClickListener{
+            (Toast.makeText( this,"Aún no está activo",Toast.LENGTH_SHORT)).show()
+        }
+    }
+
+}
