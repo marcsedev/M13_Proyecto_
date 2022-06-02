2e538bb20533453fc33144bf6f945a7f0fe81af0
lmonf <l.monfortgomez@gmail.com>
2022-06-02 22:03:58 +0200

    he añadido un comentario

--- app/src/main/java/com/example/m13_proyecto/adapter/AlumnadoAdapter.kt
+++ app/src/main/java/com/example/m13_proyecto/adapter/AlumnadoAdapter.kt
@@ -7,7 +7,8 @@ import com.example.m13_proyecto.R
 import com.example.m13_proyecto.Student
 
 //estos son los métodos
-class AlumnadoAdapter (private val studentList:List<Student>,) : RecyclerView.Adapter<AlumnadoViewHolder>(){
+class AlumnadoAdapter (private val studentList:List<Student>) : RecyclerView.Adapter<AlumnadoViewHolder>(){
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlumnadoViewHolder {
         val layoutInflater=LayoutInflater.from(parent.context)
         return AlumnadoViewHolder(layoutInflater.inflate(R.layout.item_student, parent,false))
@@ -20,7 +21,9 @@ class AlumnadoAdapter (private val studentList:List<Student>,) : RecyclerView.Ad
 
     override fun getItemCount(): Int =studentList.size
 
-//cambio2
+//cambio
+
+
 
 }
 
