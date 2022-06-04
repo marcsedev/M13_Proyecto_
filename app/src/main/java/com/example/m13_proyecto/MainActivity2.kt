e845f727eab87f17e6f7589c627861f7c820d448
MarcSogas <marc.sogas@gmail.com>
2022-06-04 14:36:37 +0200

    activity2

--- app/src/main/java/com/example/m13_proyecto/MainActivity2.kt
+++ app/src/main/java/com/example/m13_proyecto/MainActivity2.kt
@@ -1,11 +1,12 @@
 package com.example.m13_proyecto
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
+    // marc3
 }
